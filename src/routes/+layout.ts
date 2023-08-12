3f1ba8a88916d35e6fb41709db310e3fb8b791ff
Gabriel Rodríguez <rgabriel@mit.edu>
2023-08-12 18:22:15 -0400

    deploy to scripts

---
+++ src/routes/+layout.ts
@@ -0,0 +1,14 @@
+// This should make it possible to deploy on scripts,
+// by not requiring a backend
+
+// The harming SEO probably doesn't matter, since everything
+// requires login anyway. In fact it might be better that 
+// random lists don't end up on search results (but robots.txt
+// would work for that purpose too)
+
+// TODO: is this really necessary?
+
+// https://kit.svelte.dev/docs/single-page-apps#prerendering-individual-pages
+// TODO: This can also be more fine-grained
+
+export const ssr = false;
